1afe379300bb8097c204485a2987cfd6763523ab
Park Juhyun <wngus6429@gmail.com>
2024-10-18 00:44:28 +0900

    dd

--- memo-trello/src/index.tsx
+++ memo-trello/src/index.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { RecoilRoot } from 'recoil';
 
-const queryClient = new QueryClient();
-
-// 큰 회사면 textColor, borderColor, linkColor, linkHoverColor 등등 다양
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </RecoilRoot>
+    <App />
   </React.StrictMode>
 );
 
